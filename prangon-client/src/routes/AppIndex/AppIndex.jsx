634bddb67165770adef9179288a30907e71b1eb9
Sayeedur Rahman <sayeedurrahman442@gmail.com>
2024-04-25 21:10:08 +0600

    prangon-search: full-text search

--- prangon-client/src/routes/AppIndex/AppIndex.jsx
+++ prangon-client/src/routes/AppIndex/AppIndex.jsx
@@ -11,99 +11,122 @@ import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 
 import Stack from "@mui/material/Stack";
+import Skeleton from "@mui/material/Skeleton";
 
 import PostCard from "../../components/PostCard";
 import CreateIcon from "@mui/icons-material/Create";
-import Fab from '@mui/material/Fab';
+import Fab from "@mui/material/Fab";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function AppIndex() {
+    const { user } = useAuthContext();
     const navigateTo = useNavigate();
-    const {isLoading, isError, error, data} = useQuery({
+    const { isLoading, isError, error, data } = useQuery({
         queryKey: ["posts"],
         queryFn: async () => {
-            const res = await fetch("", {
+            const res = await fetch(`${import.meta.env.VITE_API_GET_POSTS}`, {
                 headers: {
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                }
+                    Authorization: `Bearer ${user.token}`,
+                },
             });
             return res.json();
-        }
-        // onSuccess: () => {}
-    })
+        },
+        onSuccess: (data) => {
+            console.log("Success", data);
+        },
+    });
+
+    console.log(data);
 
     return (
         <>
-            <Fab onClick={e => navigateTo("post")} sx={{ position: "fixed", bottom: "16px", right: "16px"}}>
-                <CreateIcon fontSize="medium" />
-            </Fab>
-            <Box
-                component="section"
-                sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                }}
-            >
-                <IconButton
-                    size="large"
-                    aria-label="Direct Messeges"
-                    color="inherit"
-                    onClick={(e) => navigateTo("dms")}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        flexGrow: 1,
-                    }}
-                >
-                    <ChatIcon fontSize="large" />
-                    <Typography sx={{ fontSize: "12px" }}>DMs</Typography>
-                </IconButton>
-                <IconButton
-                    size="large"
-                    aria-label="groups"
-                    color="inherit"
-                    onClick={(e) => navigateTo("groups")}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        flexGrow: 1,
-                    }}
-                >
-                    <GroupIcon fontSize="large" />
-                    <Typography sx={{ fontSize: "12px" }}>Groups</Typography>
-                </IconButton>
-                <IconButton
-                    size="large"
-                    aria-label="sections"
-                    color="inherit"
-                    onClick={(e) => navigateTo("sections")}
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        flexGrow: 1,
-                    }}
-                >
-                    <WorkspacesIcon fontSize="large" />
-                    <Typography sx={{ fontSize: "12px" }}>
-                        Section chat
-                    </Typography>
-                </IconButton>
-            </Box>
-            
-            <Container sx={{ my: 2, px: 0 }} maxWidth="sm">
-                <Stack direction="column" spacing={2}>
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                    <PostCard />
-                </Stack>
-            </Container>
-            
+            {isLoading ? (
+                <span>Hold your horses...</span>
+            ) : (
+                <>
+                    <Fab
+                        onClick={(e) => navigateTo("post")}
+                        sx={{
+                            position: "fixed",
+                            bottom: "16px",
+                            right: "16px",
+                        }}
+                    >
+                        <CreateIcon fontSize="medium" />
+                    </Fab>
+                    <Box
+                        component="section"
+                        sx={{
+                            p: 2,
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="Direct Messeges"
+                            color="inherit"
+                            onClick={(e) => navigateTo("dms")}
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                flexGrow: 1,
+                            }}
+                        >
+                            <ChatIcon fontSize="large" />
+                            <Typography sx={{ fontSize: "12px" }}>
+                                DMs
+                            </Typography>
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            aria-label="groups"
+                            color="inherit"
+                            onClick={(e) => navigateTo("groups")}
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                flexGrow: 1,
+                            }}
+                        >
+                            <GroupIcon fontSize="large" />
+                            <Typography sx={{ fontSize: "12px" }}>
+                                Groups
+                            </Typography>
+                        </IconButton>
+                        <IconButton
+                            size="large"
+                            aria-label="sections"
+                            color="inherit"
+                            onClick={(e) => navigateTo("sections")}
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                flexGrow: 1,
+                            }}
+                        >
+                            <WorkspacesIcon fontSize="large" />
+                            <Typography sx={{ fontSize: "12px" }}>
+                                Section chat
+                            </Typography>
+                        </IconButton>
+                    </Box>
+
+                    {data && <Container sx={{ my: 2, px: 0 }} maxWidth="sm">
+                        <Stack direction="column" spacing={2}>
+                            <PostCard />
+                            <PostCard />
+                            <PostCard />
+                            <PostCard />
+                            <PostCard />
+                            <PostCard />
+                            <PostCard />
+                            <PostCard />
+                        </Stack>
+                    </Container>}
+                </>
+            )}
         </>
     );
 }
---
+++ prangon-search/server.js
@@ -0,0 +1,109 @@
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+// Replace the uri string with your connection string.
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri);
+
+// async function run() {
+//     try {
+//         const database = client.db("sample_mflix");
+//         const movies = database.collection("movies");
+//         // Query for a movie that has the title 'Back to the Future'
+//         const query = { title: "Back to the Future" };
+//         const movie = await movies.findOne(query);
+//         console.log(movie);
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/search", async (req, res) => {
+    const userQuery = req.query.query;
+
+    if (!userQuery || userQuery.length < 2) {
+        res.json([]);
+        return;
+    }
+
+    const db = client.db("test");
+    let collection = db.collection("users");
+
+    const userSearchPipeline = [];
+    const postSearchPipeline = [];
+
+    userSearchPipeline.push({
+        $search: {
+            index: "searchUsers",
+            text: {
+                query: userQuery,
+                path: ["username", "name", "email"],
+                fuzzy: {},
+            },
+        },
+    });
+
+    userSearchPipeline.push({
+        $project: {
+            _id: 0,
+            score: { $meta: "searchScore" },
+            name: 1,
+            email: 1,
+            username: 1,
+            profile_picture: 1,
+            createdAt: 1,
+        },
+    });
+
+    const userSearchResult = await collection
+        .aggregate(userSearchPipeline)
+        .sort({ score: -1 })
+        .limit(10);
+    const UserResults = await userSearchResult.toArray();
+
+    collection = db.collection("posts");
+
+    postSearchPipeline.push({
+        $search: {
+            index: "searchPosts",
+            text: {
+                query: userQuery,
+                path: ["username", "author_name", "post_text_content"],
+                fuzzy: {},
+            },
+        },
+    });
+
+    postSearchPipeline.push({
+        $project: {
+            score: { $meta: "searchScore" },
+            _id: 1,
+            username: 1,
+            author_name: 1,
+            post_text_content: 1,
+            post_image_content: 1,
+            createdAt: 1,
+        },
+    });
+
+    const postSearch = await collection
+        .aggregate(postSearchPipeline)
+        .sort({ score: -1 })
+        .limit(10);
+    const postResults= await postSearch.toArray();
+
+    res.json({ UserResults, postResults});
+});
+
+app.listen(4001, () => {
+    console.log("Server is running on port 4001");
+});
