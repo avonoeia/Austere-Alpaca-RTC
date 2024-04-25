bb97c04cb3fe8b59687b7603cac2d1fb14ce1f67
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-04-25 18:15:50 +0600

    update app.js

--- prangon-chat/frontend/src/App.js
+++ prangon-chat/frontend/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+
+import chatpage from './pages/chatpage';
 
 function App() {
   return (
     <div className="App">
+      <Route path='/chats' component = {chatpage} />
     </div>
   );
 }
