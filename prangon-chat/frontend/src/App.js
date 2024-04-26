0881ca5f6f599f0ed0b4c91489ad729e92b945d4
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-04-26 08:46:58 +0600

    Add ability to login and homepage usage

--- prangon-chat/frontend/src/App.js
+++ prangon-chat/frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 
+import homepage from './pages/homepage';
 import chatpage from './pages/chatpage';
 
 function App() {
   return (
     <div className="App">
+      <Route path='/'     component = {homepage} exact/>
       <Route path='/chats' component = {chatpage} />
     </div>
   );
